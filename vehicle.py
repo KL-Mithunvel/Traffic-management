44807bba089f34bcb1da1243cc479f0224b48ec1
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2024-09-05 10:58:13 +0530

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -1,12 +1,15 @@
 class Vehicles:
-    def __init__(self,spawn ,home ,spd):
+    def __init__(self,spawn ,home ,spd ,vehicle_type):
         #place of birth
         self.spawn = spawn
         #place of death
         self.home = home
         self.speed = spd
+        self.vehicle_type = vehicle_type
+        
 
 
 
     def move(self):
-
+        #Movement logic 
+        pass
