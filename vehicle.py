1bc73eb815bcf05e7f4b75420824982eb20707e4
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2024-09-05 10:52:36 +0530

    vehicles added

---
+++ vehicle.py
@@ -0,0 +1,12 @@
+class Vehicles:
+    def __init__(self,spawn ,home ,spd):
+        #place of birth
+        self.spawn = spawn
+        #place of death
+        self.home = home
+        self.speed = spd
+
+
+
+    def move(self):
+
