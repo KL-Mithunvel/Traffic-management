8d9d238fa5ccdd9ed37317e7402ce626f44d8388
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2024-09-05 20:20:14 +0530

    class building

--- road.py
+++ road.py
@@ -1,3 +1,7 @@
 class road:
-    def __init__(self, l_side, r_side, ):
-        
+    def __init__(self, length):
+        self.length=length
+        self.road_pos = [[] for i in range(self.length)]
+
+    def V_spawn (self,vehicle):
+        self.road_pos .append()
